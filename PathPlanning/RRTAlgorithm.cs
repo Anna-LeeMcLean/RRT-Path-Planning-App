3fda7c86c9fc43fc2e71744b7f9791112e15998d
Anna-Lee McLean <mcleanannalee@gmail.com>
2021-12-04 23:53:58 -0500

    Added the CheckCollision() Method

--- PathPlanning/RRTAlgorithm.cs
+++ PathPlanning/RRTAlgorithm.cs
@@ -9,13 +9,13 @@ namespace PathPlanning
     // Returns: A generic list of nodes which make up the roadmap
     class RRTree
     {
-        LinkedList<double[]> roadmap;
-        public double[] start = new double[2]; 
-        public double[] end = new double[2];
-        public double stepSize;
+        List<PointF> roadmap;
+        public PointF start = new PointF(); 
+        public PointF end = new PointF();
+        public float stepSize;
         public int treeSize;
 
-        public RRTree(double[] start_, double[] end_, double stepSize_, int treeSize_)
+        public RRTree(PointF start_, PointF end_, float stepSize_, int treeSize_)
         {
             start = start_;
             end = end_;
@@ -23,8 +23,8 @@ namespace PathPlanning
             treeSize = treeSize_;
 
             // Initialize RRT with start and end coordinates
-            LinkedList<double[]> roadmap = new LinkedList<double[]>();
-            roadmap.AddFirst(start);
+            List<PointF> roadmap = new List<PointF>();
+            roadmap.Add(start);
             //roadmap.AddLast(end);
         }
 
@@ -34,59 +34,98 @@ namespace PathPlanning
             while (T.roadmap.Count < T.treeSize)
             {
                 // Initializing a new sample vector for the GenerateNewSample() method
-                double[] newSampleVector = new double[4];
+                float[] newSampleVector = new float[4];
                 List<Rectangle> obstacleList = new List<Rectangle>();
 
                 
                 GenerateNewSample(T);
-                // The method above returns double[] newSampleVector which overwrites the initailized variable and is passed to the method below.
-                CheckCollisionFree(newSampleVector, obstacleList);
+                // The method above returns float[] newSampleVector which overwrites the initailized variable and is passed to the method below.
+                if (CheckCollisionFree(newSampleVector, obstacleList))
+                {
+                    // DrawEdge();
+                    PointF newNode = new PointF(newSampleVector[3], newSampleVector[4]);
+                    T.roadmap.Add(newNode);
+                    
+                }
             }
         }
-        // I want to pass Point objects instead of double[] so badly.
+
         // ---------------------------------------------------------------------------------------------------------------------------------------
-        public static double[] GenerateNewSample(RRTree T_)
+        public static float[] GenerateNewSample(RRTree T_)
         {
             // Create x and y coords for a new sample between 0 and 5 (the dimensions of the environment)
             // Sample a random coordinate (node) in the environment
-            Random sample = new Random();
-            double xSample = sample.NextDouble() * 5;    
-            double ySample = sample.NextDouble() * 5;
-            double minDistance = double.PositiveInfinity;
+            Random randomFloat = new Random();
+            float xSample = (float)(randomFloat.NextDouble() * 5);    
+            float ySample = (float)(randomFloat.NextDouble() * 5);
+            PointF sample = new PointF(xSample, ySample);
+            float minDistance = float.PositiveInfinity;
             // Find the nearest node in the roadmap of the tree T which is closest to the random sampled coordinate
-            double xNearest = 0;
-            double yNearest = 0;
+            float xNearest = 0;
+            float yNearest = 0;
 
-            foreach(double[] node in T_.roadmap)
+            foreach(PointF node in T_.roadmap)
             {
-                double distance = Math.Sqrt(Math.Pow((xSample - node[1]),2) + Math.Pow((ySample - node[2]), 2));
+                float distance = (float)Math.Sqrt(Math.Pow(sample.X - node.X, 2) + Math.Pow(sample.Y - node.Y, 2));
                 if (distance < minDistance)
                 {
                     minDistance = distance;
-                    xNearest = node[1];
-                    yNearest = node[2];
+                    xNearest = node.X;
+                    yNearest = node.Y;
                 }
             }
 
             // Generate a new node that creates a vector from the nearest node to the random sample but is the length of stepSize.
-            double xNew = xNearest + (((xSample - xNearest) * T_.stepSize) / minDistance);
-            double yNew = yNearest + (((ySample - yNearest) * T_.stepSize) / minDistance);
+            float xNew = xNearest + (((xSample - xNearest) * T_.stepSize) / minDistance);
+            float yNew = yNearest + (((ySample - yNearest) * T_.stepSize) / minDistance);
 
-            double[] newSampleVector = { xNearest, yNearest, xNew, yNew };
+            float[] newSampleVector = { xNearest, yNearest, xNew, yNew };
 
             return newSampleVector;
         }
 
-
         // ---------------------------------------------------------------------------------------------------------------------------------------
-        public static bool CheckCollisionFree(double[] newSampleVector_, List<Rectangle> obstacleList_)
+        public static bool CheckCollisionFree(float[] newSampleVector_, List<Rectangle> obstacleList_)
         {
-            // Generate an equation for the line which connects the nearest node and new node
+            // Generate parametric equations for the line which connects the nearest node and new node
+
+            float dx1 = newSampleVector_[2] - newSampleVector_[0]; // change in x for the sample vector
+            float dy1 = newSampleVector_[3] - newSampleVector_[1]; // change in y for the sample vector
 
             foreach (Rectangle obstacle in obstacleList_)
             {
+                // Generate coordinates for each line: { xstart, ystart, xend, yend }
+
+                int[] topLine = { obstacle.Left, obstacle.Top, obstacle.Right, obstacle.Top };
+                int[] bottomLine = { obstacle.Left, obstacle.Bottom, obstacle.Right, obstacle.Bottom };
+                int[] leftLine = { obstacle.Left, obstacle.Bottom, obstacle.Left, obstacle.Top };
+                int[] rightline = { obstacle.Right, obstacle.Bottom, obstacle.Right, obstacle.Top };
+
+                // Add lines to a generic list
+                List<int[]> rectangleLines = new List<int[]>();
+
+                rectangleLines.Add(topLine); rectangleLines.Add(bottomLine);
+                rectangleLines.Add(rightline); rectangleLines.Add(leftLine);
+
+                // Initialize parametric variables t1 and t2
+                float t1 = 0; float t2 = 0;
+                
+                foreach (int[] line in rectangleLines)
+                {
+                    float dx2 = line[2] - line[0];
+                    float dy2 = line[3] - line[1];
+
+                    float denominator = dy1 * dx2 - dx1 * dy2;
+                    t1 = ((newSampleVector_[0] - line[0]) * dy2 + (line[1] - newSampleVector_[1]) * dx2) / denominator;
+                    t2 = ((line[0] - newSampleVector_[0]) * dy1 + (newSampleVector_[1] - line[1]) * dx1)/ -denominator;
+
+                }
+                
+                if ((t1 >= 0) && (t1 <= 1) && (t2 >= 0) && (t2 <= 1)) { return true; }
 
             }
+
+            return false;
         }
 
     }
